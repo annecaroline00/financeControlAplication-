b92a8327e2cb108ebec0d6c5153f2481aafd2448
jonatas.laet <jonataslaetprogramador@gmail.com>
2022-09-05 12:02:00 -0300

    Implementação de desbloqueio de CORS

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.v1.api import api_router
 from core.configs import settings
 
 app = FastAPI(
     title='Finance Control Application API'
+)
+
+origins = [
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix=settings.BASE_URL_API_V1)
